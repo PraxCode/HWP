81e0e5a33e3966de6d2f2d0d674fa34fdff23ee1
PraxCode <timo.wottka@kabelmail.de>
2022-10-20 21:13:58 +0200

    started memdump fn -> No proper format yet

--- Aufgabe3/src/memdump.c
+++ Aufgabe3/src/memdump.c
@@ -6,14 +6,37 @@ void memdump(unsigned char *str, unsigned zeilen);
 int memreplace(char *string, char cin, char cout, char **caddr);
 
 int main(int argc, char *argv[]) {
+  memdump((unsigned char *)*argv, 3);
   char *copy = malloc(argc * sizeof(char));
   strcpy(copy, *argv);
 
-  printf("%s", copy);
+  printf("\n%s", copy);
 
   free(copy);
   return 0;
 }
 
-void memdump(unsigned char *str, unsigned zeilen) {}
+void memdump(unsigned char *str, unsigned zeilen) {
+  // Header
+  printf("%s", "ADDR ");
+  // Hex
+  for (int i = 0; i < 16; i++) {
+    printf("0%X ", i);
+  }
+  // Ascii
+  for (int i = 0; i < 16; i++) {
+    printf("%X", i);
+  }
+  printf("\n");
+
+  for (int zeile = 0; zeile < zeilen; zeile++) {
+    // sizeof unnötig, da sizeof(char) = 1
+    // * 16, da 16 Byte pro Zeile im memdump
+    printf("0x%X\n", str[zeile * 16 * sizeof(char)]);
+  }
+
+  // print str[zeile]
+  // loop over chars and print
+  // Somehow manage segmention fault
+}
 int memreplace(char *string, char cin, char cout, char **caddr) { return 0; }
