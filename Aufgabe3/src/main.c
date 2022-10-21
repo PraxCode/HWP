c777efe82f33cf8a398770502c31346c0397fe49
PraxCode <timo.wottka@kabelmail.de>
2022-10-21 19:28:56 +0200

    WIP Commit

--- Aufgabe3/src/main.c
+++ Aufgabe3/src/main.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ADDR_PER_ROW 16
+
 void memdump(unsigned char *str, unsigned zeilen);
 int memreplace(char *string, char cin, char cout, char **caddr);
 
 int main(int argc, char *argv[]) {
   memdump((unsigned char *)*argv, 3);
+  printf("\n");
   char *copy = malloc(argc * sizeof(char));
   strcpy(copy, *argv);
 
@@ -15,28 +18,41 @@ int main(int argc, char *argv[]) {
   free(copy);
   return 0;
 }
-
-void memdump(unsigned char *str, unsigned zeilen) {
+void printHeader() {
   // Header
   printf("%s", "ADDR ");
   // Hex
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < ADDR_PER_ROW; i++) {
     printf("0%X ", i);
   }
   // Ascii
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < ADDR_PER_ROW; i++) {
     printf("%X", i);
   }
+
   printf("\n");
+}
+void memdump(unsigned char *str, unsigned zeilen) {
+  printHeader();
 
   for (int zeile = 0; zeile < zeilen; zeile++) {
-    // sizeof unnötig, da sizeof(char) = 1
     // * 16, da 16 Byte pro Zeile im memdump
-    printf("0x%X\n", str[zeile * 16 * sizeof(char)]);
+    printf("0x%X ", str[zeile * ADDR_PER_ROW]);
+
+    for (int c = 0; c < ADDR_PER_ROW; c++) {
+
+      char character = str[c];
+      if (character < 16) {
+        printf("0%x ", character);
+      } else {
+        printf("%x ", character);
+      }
+    }
+    printf("\n");
   }
 
   // print str[zeile]
-  // loop over chars and print
+  // loop over chars and print (format)
   // Somehow manage segmention fault
 }
 int memreplace(char *string, char cin, char cout, char **caddr) { return 0; }
