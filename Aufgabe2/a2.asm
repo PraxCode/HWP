6b630d9233bc8570548d3130e06988c1f614bd5f
PraxCode <timo.wottka@kabelmail.de>
2022-10-22 19:10:44 +0200

    A2: Calculate offset of byte array to send to output port 0

--- Aufgabe2/a2.asm
+++ Aufgabe2/a2.asm
@@ -1,34 +1,21 @@
 	org 100h
 	cpu 8086
 
-
-
+jmp START
 LED_L equ 94h
 LED_R equ 92h
 LED_h equ 90h
+; ARR Codierung für LED-Darstellung des SBCs von 0 bis F sequenziell
+ARR: db 00111111b, 00000110b, 01011011b, 01001111b, 01100110b, 01101101b, 01111101b, 00000111b, 01111111b, 01101111b, 01110111b, 01111100b, 00111001b, 01011110b, 01111001b, 01110001b
 
-ZERO	db 00111111b
-ONE	  db 00001110b
-TWO	  db 01011011b
-THREE	db 01001111b
-FOUR	db 01100110b
-FIVE	db 01101101b
-SIX	  db 01111101b
-SEVEN db 00000111b
-EIGHT	db 01111111b
-NINE	db 01101111b
-A	    db 01110111b
-B	    db 01111100b
-C	    db 00111001b
-D	    db 01011110b
-E	    db 01111001b
-F	    db 01110001b
-	
-START:
-  in al, 0; Port 0 in al ; z. B. 0000'0000
-  out 0, al
-  mov ah, ZERO
-  add al, ah; Calculate Offset
+START: 
+  in al, 00h; Port 0 in al ; z. B. 0000'0000
+  out 00h, al
+
+  ; Calc Offset 
+  mov ah, al
+  lea bx, ARR
+  xlat
 
   out 90h, al
 	jmp START
