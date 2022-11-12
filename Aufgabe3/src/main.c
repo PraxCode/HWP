ca05ddccda6a51927262bcc42a095a420fc42ad3
Prax <timo.wottka@kabelmail.de>
2022-11-12 20:29:50 +0100

    findstring - somehow doesn't return NULL if string not found

--- Aufgabe3/src/main.c
+++ Aufgabe3/src/main.c
@@ -1,14 +1,11 @@
-#include <assert.h>
 #include <malloc.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 #define ADDR_PER_ROW 16
-#define ROWS 3
 
 void memdump(unsigned char *memblock, unsigned length);
-
 int findstring(char **string_array, char *searched_string);
 
 int main(int argc, char **argv)
@@ -19,16 +16,21 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    int length = strlen(argv[1]) + 1;
+    size_t length = strlen(argv[1]) + 1;
     char *string = malloc(length * sizeof(char));
     strcpy(string, argv[1]);
 
     memdump((unsigned char *) string, length);
     printf("\n");
 
-    char* searched = argv[2];
+    char *searched = argv[2];
     int count = findstring(&string, searched);
-    printf("Addresse vom vorletzten gefundenen Teilstring: %s.\nAnzahl der Vorkommen: %i", searched,count);
+    printf("After Function: %p::0x%p\n", searched, &searched);
+    if (searched != NULL){
+        printf("Address of penultimate substring, that has been found: 0x%p.\nCount of substring-occurrences: %i", searched, count);
+    } else {
+        printf("%s", "NULL");
+    }
     free(string);
     return 0;
 }
@@ -75,23 +77,32 @@ void memdump(unsigned char *memblock, unsigned length)
 
 int findstring(char **string_array, char *searched_string)
 {
-    char* string = *string_array;
+    size_t length_of_searched = strlen(searched_string);
+    if (length_of_searched == 0){
+        searched_string = NULL;
+        return 0;
+    }
+
+    char *string = *string_array;
     int occurrences = 0;
+    char *last_addr = NULL;
+    char *penultimate_addr = NULL;
 
-    char* last_addr = NULL;
-    char* penultimate_addr = NULL;
+    char *substring = string;
+    while (substring)
+    {
+        // strstr() returns address of first substring-occurrence,
+        // so the ptr needs to be moved by the length of the searched string
+        substring = strstr(substring + length_of_searched, searched_string);
+        if (!substring)
+            break;
 
-    while (*string != '\0'){
-        if (*string == *searched_string)
-        {
-            occurrences++;
-            penultimate_addr = last_addr;
-            last_addr = string;
-        }
-
-        string++;
+        occurrences++;
+        penultimate_addr = last_addr;
+        last_addr = substring;
     }
 
     searched_string = penultimate_addr;
+    printf("\nIn Function: 0x%p::0x%p\n", searched_string, penultimate_addr);
     return occurrences;
 }
