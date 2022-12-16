117da93ad2e69946a040877d5e18576c559d64b2
PraxCode <timo.wottka@kabelmail.de>
2022-12-16 19:10:26 +0100

    result should work - uncertain about flags

--- Aufgabe5/source/addsub.asm
+++ Aufgabe5/source/addsub.asm
@@ -1,18 +1,35 @@
-cpu 8086
+
+section .text
 
 global addsub
 
 addsub:
-  push bp
-  mov bs, sp
- sub sp, 2+2+1+4 ; Allocate space for 
-                  ; short num1, short num2, char operation, ushort* flags
-   
-  
+    push ebp            ; Save the value of ebp on the stack
+    mov ebp, esp        ; Set ebp to the current value of esp
+    push ebx            ; Save ebx on the stack
 
-  
-  
-  pop bp
-  ret
+    mov ebx, [ebp+8]
+    cmp byte [ebp+16], 2bh ; Plus
+    jz add              
+    cmp byte [ebp+16], 2dh ; Minus
+    jz sub              
 
+add:
+    add ebx, [ebp+12]
+    jmp cleanup          
 
+sub:
+    sub ebx, [ebp+12]   ; Subtract num2 from num1 and store the result in ebx
+    jmp cleanup; Jump to end label
+
+cleanup:
+    pushfd
+    pop ecx
+    mov edx, [ebp + 20]
+    mov [edx], ecx ; Write in flags
+
+    mov eax, ebx
+
+    mov esp, ebp
+    pop ebp
+    ret                 
--- Aufgabe5/source/main.c
+++ Aufgabe5/source/main.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 extern int addsub(int num1, int num2, char operation, unsigned short *flags);
 
@@ -10,32 +12,39 @@ typedef struct {
   int result;
 } result_info;
 
-void build_result_info(result_info *ri, char **argv) {
-  ri->num1 = atoi(argv[0]);
-  ri->operation = atoi(argv[1]);
-  ri->num2 = atoi(argv[2]);
+void init_result_info(result_info *ri, char **argv) {
+  ri->num1 = atoi(argv[1]);
+  ri->operation = argv[2][0];
+  ri->num2 = atoi(argv[3]);
+  ri->flags = malloc(sizeof(unsigned short));
 
   ri->result = addsub(ri->num1, ri->num2, ri->operation, ri->flags);
 }
-/*
-void print_flags(unsigned short *flags) {}
-void cite_signed(result_info *ri) {}
-void cite_unsigned(result_info *ri) {}
-*/
+
+void print_flags(unsigned short *flags) {
+  char str[16];
+  sprintf(str, "%b", *flags);
+  printf("Flags:\nODITSZAPC\n%s\n", str);
+}
+
+void cite_signed(result_info *ri) {
+}
+
+void cite_unsigned(result_info *ri) {
+}
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
+  if (argc != 4) {
+    fprintf(stderr, "Invalid use! Should be: <number1> <operation> <number2>");
     return -1;
   }
 
   result_info ri;
-  build_result_info(&ri, argv);
+  init_result_info(&ri, argv);
 
-  /*
   print_flags(ri.flags);
   cite_signed(&ri);
-  cite_unsigned(&ri);
-  */
 
+  free(ri.flags);
   return 0;
 }
